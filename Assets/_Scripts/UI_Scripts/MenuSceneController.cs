d9c8b7d5eea2531e24113f51f794ef67e1f8913d
Faim Khandker <faim.khandker@gmail.com>
2017-05-01 23:05:58 -0400

    Commit 3 – UI Set Up
    Added MenuSceneController script, modified and designed MenuScene, added proper object referencing to script and UI components

---
+++ Assets/_Scripts/UI_Scripts/MenuSceneController.cs
@@ -0,0 +1,49 @@
+﻿/*
+Khandker Faim Hussain
+Thurs 12/10/2015
+	Description:		Code from "Survival Shooter" and Assignment 03
+	Revision History:	IDK!!!...
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneController : MonoBehaviour 
+{
+	//PUBLIC INSTANCE VARIABLES
+	//NOTE: These declared public "Button" objects will be referenced by the button's child objects which are the text objects in Unity
+	//because I added button components on the text objects to make them look pretty...
+	public Button btnStart;
+	public Button btnInfo;
+	public Button btnCheats;
+	public Button btnExit;
+
+
+//	// Use this for initialization
+//	void Start () 
+//	{
+//	}
+	
+	//PUBLIC METHODS - CHANGING SCENES VIA BUTTON CLICKS (Button UI objects will call these methods "OnClick")
+	public void MainLevelScene()
+	{
+		SceneManager.LoadScene ("MainLevelScene");
+	}
+
+	public void InfoScene()
+	{
+		SceneManager.LoadScene ("InfoScene");
+	}
+
+	public void CheatsScene()
+	{
+		SceneManager.LoadScene ("CheatsScene");
+	}
+
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+}
